c39968482f59d7c67588ed7d46f9e0dfbd17ad06
107982913+qwerdenkerXD <107982913+qwerdenkerXD@users.noreply.github.com>
2023-09-10 11:10:40 +0200

    init.sh: added GitHub CLI, neovim removed 'starship preset ...' bc starship.toml is inside .config

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 My personal UNIX config
 
 After setting up a new Debian-based Linux, run: (not tested yet)
+
 `curl -o- https://raw.githubusercontent.com/qwerdenkerXD/dotfiles/master/init.sh | sh`
--- init.sh
+++ init.sh
@@ -8,12 +8,11 @@ fi
 . .bash_aliases
 
 update
-sudo apt install python3-pip texlive-full r-base zsh chroma autojump cargo
+sudo apt install python3-pip texlive-full r-base zsh chroma autojump cargo neovim
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 export PATH=$HOME/.cargo/bin:$PATH
 cargo install lsd starship
-starship preset nerd-font-symbols -o .config/starship.toml
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash  # install nvm
 . .nvm/nvm.sh
 nvm install node
@@ -29,6 +28,16 @@ git init
 git remote add origin https://github.com/qwerdenkerXD/dotfiles
 git pull
 rm -r .git
+
+# install GitHub CLI
+type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
+
+gh auth login
 
 # for nerd fonts in shell, copy the .ttf files to C:\Windows\Fonts and select the font in terminal settings
 # in true linux (not wsl), copy it to /.local/share/fonts and run fc-cache -fv and then select it for terminal
