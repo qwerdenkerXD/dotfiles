e5b5e7e6e71fef7243acec8d683e05db75363304
Franz-Eric Sill <107982913+qwerdenkerXD@users.noreply.github.com>
2024-09-19 11:57:30 +0200

    fixed script

--- init.sh
+++ init.sh
@@ -5,19 +5,18 @@ if [ -f ./init.sh ]; then   # if this script is run multiple times, it breaks af
     exit
 fi
 
-. .bash_aliases
-
-update
-sudo apt install python3-pip texlive-full r-base zsh chroma autojump cargo neovim
+sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
+sudo apt install -y python3-pip texlive-full r-base zsh chroma autojump cargo neovim cmake
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+sudo apt remove -y rust && sudo apt autoremove -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 export PATH=$HOME/.cargo/bin:$PATH
 cargo install lsd starship
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash  # install nvm
 . .nvm/nvm.sh
 nvm install node
 nvm install-latest-npm
-pip3 install pycodestyle matplotlib numpy
+sudo apt install -y python3-pycodestyle python3-matplotlib python3-numpy python3-scipy python3-tqdm
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git .oh-my-zsh/plugins/zsh-syntax-highlighting
 
 if [ -f ~/.bash_profile ]; then   # nvm-installer creates it, but .bashrc won't be loaded then
@@ -26,11 +25,10 @@ fi
 
 git init
 git remote add origin https://github.com/qwerdenkerXD/dotfiles
-git pull
-rm -r .git
+rm .bashrc .zshrc && git pull origin master
+sudo rm -r .git
 
 # install GitHub CLI
-type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
 && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
 && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
